54e75d745a3c3c6c8bbca3c27738c4a25a856bcf
Alireza <ar.rousta77@gmail.com>
2022-12-01 19:39:36 +0330

    SECTION4 added

--- index.php
+++ index.php
@@ -7,40 +7,51 @@
         <section id="section1">
             <div class="container" >
                 <div class="row">
-                    <div class="col">
+                    <div class="col-5" id="image_col">
                         <img src="http://localhost/wordpress.developer/wp-content/themes/iclinic_theme/assets/images/Apple_watch-series7-availability_hero_10052021_big.jpg-1.png" 
                          width=536px height=435px alt="applewatch">
                     </div>
-                    <div class="col" id="header_title">
-                        <h1>اپل واچ سری 7</h1>
-                        <h6> دنیای هوشمند در سری جدید ساعت های اپل </h6>
-                        <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد،</p>
+                    <div class="col-7" id="header_title">
+                        <h1>اپل واچ سری ۷</h1>
+                        <h6 id="header_title_h6"> دنیای هوشمند در سری جدید ساعت های اپل </h6>
+                        <p id="header_title_p">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد،</p>
 
                         <div class="row">
                             <div class="col" id="header_2">
-                                <div class="row">
-                                    <div class="col w-auto p-3">
-                                        <img src="http://localhost/wordpress.developer/wp-content/uploads/2022/11/F07H3_AV1-1.png" alt="applewtch">
-                                    </div>
-                                    <div class="col">
-                                        <h6>اپل واچ سری ۶</h6>
-                                        <p>رنگ مشکی، ۴۴ سانتی متر</p>
-                                    </div>
-                                </div>
+                                
+
+                                    <button type="button" class="btn btn-light" id="btn1">
+                                        <div class="flex-container" id="btn_flex">
+                                            <div class="flex-item" id="btn_flex_item1">
+                                                <img src="http://localhost/wordpress.developer/wp-content/uploads/2022/11/F07H3_AV1-1.png" alt="applewtch" id="applewtch_img">
+                                            </div>
+
+                                            <div class="flex-item" id="btn_flex_item2">
+                                                <h6 id="btn_flex_item2_h6">اپل واچ سری ۶</h6>
+                                                <p id="btn_flex_item2_p1">رنگ مشکی، ۴۴ سانتی متر</p>
+                                                <p id="btn_flex_item2_p2">۸،۷۷۰،۰۰۰ تومان</p>
+                                            </div>
+                                                                                   
+                                        </div>
+                                    </button>
+                                    
+                                    <button type="button" class="btn btn-light" id="btn1">
+                                        <div class="flex-container" id="btn_flex">
+                                            <div class="flex-item" id="btn_flex_item1">
+                                                <img src="http://localhost/wordpress.developer/wp-content/uploads/2022/11/F07H3_AV1-1.png" alt="applewtch" id="applewtch_img">
+                                            </div>
+                                            
+                                            <div class="flex-item" id="btn_flex_item2">
+                                                <h6 id="btn_flex_item2_h6">اپل واچ سری ۶</h6>
+                                                <p id="btn_flex_item2_p1">رنگ مشکی، ۴۴ سانتی متر</p>
+                                                <p id="btn_flex_item2_p2">۸،۷۷۰،۰۰۰ تومان</p>
+                                            </div>
+                                                                                   
+                                        </div>
+                                    </button>
+
                             </div>
 
-                            <div class="col" id="header_2">
-                                <div class="row">
-                                        <div class="col w-auto p-3">
-                                            <img src="http://localhost/wordpress.developer/wp-content/uploads/2022/11/F07H3_AV1-1.png" alt="applewtch">
-                                        </div>
-                                        <div class="col">
-                                            <h6>اپل واچ سری ۶</h6>
-                                            <p>رنگ مشکی، ۴۴ سانتی متر</p>
-
-                                        </div>
-                                    </div>
-                            </div>
                         </div>
                     </div>
                 </div>
@@ -69,6 +80,172 @@
                         </div>
                     </div>
                 </div>
+            </div>
+
+        </section>
+
+        <section>
+            <div class="flex-container wrap" id="section4_flex-item1">
+                <div>
+                    <button>
+                        <img src="http://localhost/wordpress.developer/wp-content/themes/iclinic_theme/assets/images/MN643.png" alt="iphone1">
+                        <p>لوازم جانبی اپل</p>
+                    </button>
+                </div>
+                <div>
+                    <button>
+                        <img src="http://localhost/wordpress.developer/wp-content/themes/iclinic_theme/assets/images/compare_mbp14_and_16__f2dhysusb5im_large_2x.png" alt="laptop1">
+                        <p>دستگاه‌های‌مک</p>
+                    </button>
+                </div>
+                <div>
+                    <button>
+                        <img src="http://localhost/wordpress.developer/wp-content/themes/iclinic_theme/assets/images/compare_iphone_13_pro__bpn3x8hs692a_large_2x.png" alt="iphon2">
+                        <p>آیفون</p>
+                    </button>
+                </div>
+                <div>
+                    <button>
+                        <img src="http://localhost/wordpress.developer/wp-content/themes/iclinic_theme/assets/images/compare_ipad_pro__erf9x8mw04sy_large_2x.png" alt="ipad">
+                        <p>آی‌پد</p>
+                    </button>
+                </div>
+                <div>
+                    <button>
+                        <img src="http://localhost/wordpress.developer/wp-content/themes/iclinic_theme/assets/images/compare_s7__dqi9jnwa4sq6_large_2x.png" alt="appw">
+                        <p>اپل واچ</p>
+                    </button>
+                </div>
+                <div>
+                    <button>
+                        <img src="http://localhost/wordpress.developer/wp-content/themes/iclinic_theme/assets/images/compare_airpods_pro__e9uzt0mzviem_large_2x.png" alt="airp">
+                        <p>ایرپاد</p>
+                    </button>
+                </div>
+            </div>
+
+        </section>
+
+        <section>
+            <div class="flex-container wrap" id="section5_flex-item1">
+                <a href="#">
+                    <div class="flex-item">
+                        <div class="flex-container" id="inner_flex">
+                            <div class="flex-item_inner1">
+                                <img src="http://localhost/wordpress.developer/wp-content/themes/iclinic_theme/assets/images/Apple-Watch.png" alt="airp">
+                            </div>
+                            <div class="flex-item_inner2">
+                                <p id="p11">اپل واچ نایکی پلاس سری SE آلومینیوم نقره ای با بند سیلیکون سفید</p>
+                                <p id="p22">۹٬۸۷۰٬۰۰۰ تومان</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+
+                <a href="#">
+                    <div class="flex-item">
+                        <div class="flex-container" id="inner_flex">
+                            <div class="flex-item_inner1">
+                                 <img src="http://localhost/wordpress.developer/wp-content/themes/iclinic_theme/assets/images/headphon.png" alt="airp">
+                            </div>
+                            <div class="flex-item_inner2">
+                                <p id="p11">اپل واچ نایکی پلاس سری SE آلومینیوم نقره ای با بند سیلیکون سفید</p>
+                                <p id="p22">۹٬۸۷۰٬۰۰۰ تومان</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+                <a href="#">
+                    <div class="flex-item">
+                        <div class="flex-container" id="inner_flex">
+                            <div class="flex-item_inner1">
+                                <img src="http://localhost/wordpress.developer/wp-content/themes/iclinic_theme/assets/images/iphone13.png" alt="airp">
+                            </div>
+                            <div class="flex-item_inner2">
+                                <p id="p11">اپل واچ نایکی پلاس سری SE آلومینیوم نقره ای با بند سیلیکون سفید</p>
+                                <p id="p22">۹٬۸۷۰٬۰۰۰ تومان</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+                <a href="#">
+                    <div class="flex-item">
+                        <div class="flex-container" id="inner_flex">
+                            <div class="flex-item_inner1">
+                                <img src="http://localhost/wordpress.developer/wp-content/themes/iclinic_theme/assets/images/airpod.png" alt="airp">
+                            </div>
+                            <div class="flex-item_inner2">
+                                <p id="p11">اپل واچ نایکی پلاس سری SE آلومینیوم نقره ای با بند سیلیکون سفید</p>
+                                <p id="p22">۹٬۸۷۰٬۰۰۰ تومان</p>
+                            </div>
+                        </div>
+                    </div> 
+                </a>
+                <a href="#">
+                    <div class="flex-item">
+                        <div class="flex-container" id="inner_flex">
+                            <div class="flex-item_inner1">
+                                <img src="http://localhost/wordpress.developer/wp-content/themes/iclinic_theme/assets/images/iphone-13-pro-max-blue-select.png" alt="airp">
+                            </div>
+                            <div class="flex-item_inner2">
+                                <p id="p11">اپل واچ نایکی پلاس سری SE آلومینیوم نقره ای با بند سیلیکون سفید</p>
+                                <p id="p22">۹٬۸۷۰٬۰۰۰ تومان</p>
+                            </div>
+                        </div>
+                    </div> 
+                </a>
+                <a href="#">
+                    <div class="flex-item">
+                        <div class="flex-container" id="inner_flex">
+                            <div class="flex-item_inner1">
+                                <img src="http://localhost/wordpress.developer/wp-content/themes/iclinic_theme/assets/images/284147.png" alt="airp">
+                            </div>
+                            <div class="flex-item_inner2">
+                                <p id="p11">اپل واچ نایکی پلاس سری SE آلومینیوم نقره ای با بند سیلیکون سفید</p>
+                                <p id="p22">۹٬۸۷۰٬۰۰۰ تومان</p>
+                            </div>
+                        </div>
+                    </div> 
+                </a>
+                <a href="#">
+                    <div class="flex-item">
+                        <div class="flex-container" id="inner_flex">
+                            <div class="flex-item_inner1">
+                            <img src="http://localhost/wordpress.developer/wp-content/themes/iclinic_theme/assets/images/airpod.png" alt="airp">
+                        </div>
+                            <div class="flex-item_inner2">
+                                <p id="p11">اپل واچ نایکی پلاس سری SE آلومینیوم نقره ای با بند سیلیکون سفید</p>
+                                <p id="p22">۹٬۸۷۰٬۰۰۰ تومان</p>
+                            </div>
+                        </div>
+                    </div> 
+                </a>
+                <a href="#">
+                    <div class="flex-item">
+                        <div class="flex-container" id="inner_flex">
+                            <div class="flex-item_inner1">
+                                <img src="http://localhost/wordpress.developer/wp-content/themes/iclinic_theme/assets/images/iphone-13-pro-max-blue-select.png" alt="airp">
+                            </div>
+                            <div class="flex-item_inner2">
+                                <p id="p11">اپل واچ نایکی پلاس سری SE آلومینیوم نقره ای با بند سیلیکون سفید</p>
+                                <p id="p22">۹٬۸۷۰٬۰۰۰ تومان</p>
+                            </div>
+                        </div>
+                    </div> 
+                </a>
+                <a href="#">
+                    <div class="flex-item">
+                        <div class="flex-container" id="inner_flex">
+                            <div class="flex-item_inner1">
+                                <img src="http://localhost/wordpress.developer/wp-content/themes/iclinic_theme/assets/images/284147.png" alt="airp">
+                            </div>
+                            <div class="flex-item_inner2">
+                                <p id="p11">اپل واچ نایکی پلاس سری SE آلومینیوم نقره ای با بند سیلیکون سفید</p>
+                                <p id="p22">۹٬۸۷۰٬۰۰۰ تومان</p>
+                            </div>
+                        </div>
+                    </div> 
+                </a>
             </div>
 
         </section>
